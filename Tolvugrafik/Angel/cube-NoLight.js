dfda4a7277b688a1b410fb5a1e6b7ff526bbf9c6
Lavardur <antonben44@gmail.com>
2024-10-07 17:11:22 +0000

    hlutir færðir til

---
+++ Tolvugrafik/Angel/cube-NoLight.js
@@ -0,0 +1,169 @@
+/////////////////////////////////////////////////////////////////
+//    Sýnidæmi í Tölvugrafík
+//     Sýnidæmi með tening þar sem allir hnútarnir hafa sama
+//     lit.  Þá er erfitt að sjá þrívídd!
+//
+//    Hjálmtýr Hafsteinsson, október 2024
+/////////////////////////////////////////////////////////////////
+var canvas;
+var gl;
+
+var NumVertices  = 36;
+
+var points = [];
+var colors = [];
+
+var xAxis = 0;
+var yAxis = 1;
+var zAxis = 2;
+
+var axis = 0;
+var theta = [ 0, 0, 0 ];
+
+var movement = false;     // Do we rotate?
+var spinX = 0;
+var spinY = 0;
+var origX;
+var origY;
+
+var fovy = 50.0;
+var near = 0.2;
+var far = 100.0;
+
+var zDist = -3.0;
+
+var at = vec3(0.0, 0.0, 0.0);
+var up = vec3(0.0, 1.0, 0.0);
+
+var matrixLoc;
+
+window.onload = function init()
+{
+    canvas = document.getElementById( "gl-canvas" );
+    
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    colorCube();
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 0.9, 1.0, 1.0, 1.0 );
+    
+    gl.enable(gl.DEPTH_TEST);
+
+    //
+    //  Load shaders and initialize attribute buffers
+    //
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+    
+    var cBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW );
+
+    var vColor = gl.getAttribLocation( program, "vColor" );
+    gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vColor );
+
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
+
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+    matrixLoc = gl.getUniformLocation( program, "rotation" );
+    projectionLoc = gl.getUniformLocation( program, "projection" );
+
+    projectionMatrix = perspective( fovy, 1.0, near, far );
+    gl.uniformMatrix4fv(projectionLoc, false, flatten(projectionMatrix) );
+
+    //event listeners for mouse
+    canvas.addEventListener("mousedown", function(e){
+        movement = true;
+        origX = e.offsetX;
+        origY = e.offsetY;
+        e.preventDefault();         // Disable drag and drop
+    } );
+
+    canvas.addEventListener("mouseup", function(e){
+        movement = false;
+    } );
+
+    canvas.addEventListener("mousemove", function(e){
+        if(movement) {
+    	    spinY = ( spinY + (e.offsetX - origX) ) % 360;
+            spinX = ( spinX + (origY - e.offsetY) ) % 360;
+            origX = e.offsetX;
+            origY = e.offsetY;
+        }
+    } );
+    
+    // Event listener for mousewheel
+     window.addEventListener("wheel", function(e){
+         if( e.deltaY > 0.0 ) {
+             zDist += 0.2;
+         } else {
+             zDist -= 0.2;
+         }
+     }  );  
+
+    render();
+}
+
+function colorCube()
+{
+    quad( 1, 0, 3, 2 );
+    quad( 2, 3, 7, 6 );
+    quad( 3, 0, 4, 7 );
+    quad( 6, 5, 1, 2 );
+    quad( 4, 5, 6, 7 );
+    quad( 5, 4, 0, 1 );
+}
+
+function quad(a, b, c, d) 
+{
+    var vertices = [
+        vec3( -0.5, -0.5,  0.5 ),
+        vec3( -0.5,  0.5,  0.5 ),
+        vec3(  0.5,  0.5,  0.5 ),
+        vec3(  0.5, -0.5,  0.5 ),
+        vec3( -0.5, -0.5, -0.5 ),
+        vec3( -0.5,  0.5, -0.5 ),
+        vec3(  0.5,  0.5, -0.5 ),
+        vec3(  0.5, -0.5, -0.5 )
+    ];
+
+    var vertexColor = vec4( 1.0, 0.6, 0.2, 1.0 );
+
+    // We need to partition the quad into two triangles in order for
+    // WebGL to be able to render it.  In this case, we create two
+    // triangles from the quad indices
+    
+    var indices = [ a, b, c, a, c, d ];
+
+    for ( var i = 0; i < indices.length; ++i ) {
+        points.push( vertices[indices[i]] );
+    
+        // Alltaf sami liturinn 
+        colors.push(vertexColor);
+        
+    }
+}
+
+function render()
+{
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
+
+    mv = lookAt( vec3(0.0, 0.0, zDist), at, up );
+    mv = mult( mv, rotateX(spinX) );
+    mv = mult( mv, rotateY(spinY) );
+    
+    gl.uniformMatrix4fv(matrixLoc, false, flatten(mv));
+
+    gl.drawArrays( gl.TRIANGLES, 0, NumVertices );
+
+    requestAnimFrame( render );
+}
+
